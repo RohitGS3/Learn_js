399e76ef94affd04bbeb42331ac3936c278a732a
Rohit Suryawanshi <1.rohitsuryawanshi@gmail.com>
2026-02-22 13:13:55 +0530

    conversion operations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+
+console.log(typeof score) // string
+console.log(typeof(score)) // string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" - 33
+// "33abc" - NaN
+// true - 1, false - 0
+let isLoggedIn= "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
